a7a7946bbd4c8f0f111f1f7541e83d9f6a29ef67
Clemens Lange <clemens.lange@cern.ch>
2017-08-30 15:33:56 +0200

    clean up post fit script

--- VVResonances/interactive/runPostFitJJStyle_LnuJ.py
+++ VVResonances/interactive/runPostFitJJStyle_LnuJ.py
@@ -5,32 +5,27 @@ import ROOT
 ROOT.gSystem.Load("libHiggsAnalysisCombinedLimit")
 
 
+templates = ["XWW", "XWZ"]
+labels = ["X #rightarrow WW", "X #rightarrow WZ"]
+pur = ['HP', 'LP']
+bosons = ['WZ', 'WW']
+mass = 2000
 
-
-
-for c in ['WZ', 'WW']:
-# for c in ['WZ']:
-    if c=='vbf':
-        pur=['NP']
-    else:
-        pur=['HP','LP']
-        # pur=['HP']
+for c in bosons:
     for p in pur:
-        for l in ['e','mu']:
-        # for l in ['e']:
+        for l in ['e', 'mu']:
 
-            inFile = "higgsCombineTest.MaxLikelihoodFit.mH2000.{}_{}_{}_nob.root".format(l, p, c)
+            inFile = "higgsCombineTest.MaxLikelihoodFit.mH{}.{}_{}_{}_nob.root".format(mass, l, p, c)
 
-            plotter=RooPlotter(inFile)
+            plotter = RooPlotter(inFile)
             # plotter=RooPlotter("higgsCombineTest.MaxLikelihoodFit.mH2000.root")
-            plotter.fix("MH",1350)
-            plotter.fix("r",0.0)
+            plotter.fix("MH", 1350)
+            plotter.fix("r", 0.0)
             plotter.prefit()
-            plotter.addContribution("XWW",True,"X #rightarrow WW",3,1,ROOT.kOrange+10,0,ROOT.kWhite)
-            #plotter.addContribution("XWZ",True,"X #rightarrow WW",3,1,ROOT.kMagenta,0,ROOT.kWhite)
-            plotter.addContribution("QCD",False,"background",2,1,ROOT.kBlack,1001,ROOT.kAzure-9)
+            plotter.addContribution("XWW", True, "X #rightarrow WW", 3, 1, ROOT.kOrange + 10, 0, ROOT.kWhite)
+            # plotter.addContribution("XWZ",True,"X #rightarrow WW",3,1,ROOT.kMagenta,0,ROOT.kWhite)
+            plotter.addContribution("QCD", False, "background", 2, 1, ROOT.kBlack, 1001, ROOT.kAzure - 9)
             # plotter.addContribution("nonRes",False,"non resonant",2,1,ROOT.kBlack,1001,ROOT.kAzure-9,"_opt")
-
 
             # print plotter.w.Print()
             # continue
@@ -43,13 +38,13 @@ for c in ['WZ', 'WW']:
 
             # plotter.drawBinned("MJ","m_{j} (GeV)",c+"_"+l+"_"+p+"_13TeV",[],0,0,"")
             # "LNUJ_XWW_e_HP_WZ_nob_13TeV"
-            plotter.drawBinned("MVV","m_{l#nuJ} (GeV)","LNUJ_XWW_"+l+"_"+p+"_"+c+"_nob_13TeV",[],0,0,"")
-            cmslabel_prelim(plotter.pad1,'2016',11)
-            plotter.canvas.SaveAs("postFitMJJ_"+c+"_"+l+"_"+p+"_13TeV.pdf")
+            plotter.drawBinned("MVV", "m_{l#nuJ} (GeV)", "LNUJ_XWW_" + l + "_" + p + "_" + c + "_nob_13TeV", [], 0, 0, "")
+            cmslabel_prelim(plotter.pad1, '2016', 11)
+            plotter.canvas.SaveAs("postFitMJJ_" + c + "_" + l + "_" + p + "_13TeV.pdf")
 
-            plotter.drawBinned("MVV","m_{l#nuJ} (GeV)","LNUJ_XWW_"+l+"_"+p+"_"+c+"_nob_13TeV",[],0,True,"")
-            cmslabel_prelim(plotter.pad1,'2016',11)
-            plotter.canvas.SaveAs("postFitMJJ_"+c+"_"+l+"_"+p+"_13TeV_log.pdf")
+            plotter.drawBinned("MVV", "m_{l#nuJ} (GeV)", "LNUJ_XWW_" + l + "_" + p + "_" + c + "_nob_13TeV", [], 0, True, "")
+            cmslabel_prelim(plotter.pad1, '2016', 11)
+            plotter.canvas.SaveAs("postFitMJJ_" + c + "_" + l + "_" + p + "_13TeV_log.pdf")
 
 #            plotter.drawBinned("MLNuJ","m_{VV} (GeV)",c+"_"+l+"_"+p+"_13TeV",[],0,0,"MJ:sig:66:86")
 #            cmslabel_prelim(plotter.pad1,'2016',11)
@@ -93,9 +88,7 @@ for c in ['WZ', 'WW']:
             # plotter.canvas.SaveAs("postFitMVVHi_"+c+"_"+l+"_"+p+"_13TeV.eps")
 
 
-
-
-#for l in ['mu','e']:
+# for l in ['mu','e']:
 #    for p in ['both']:
 #        for c in ['vbf']:
 #            plotter.drawProjection("MJ","m_{j} [GeV]",c+"_"+l+"_"+p+"_13TeV",1,0)
@@ -104,9 +97,8 @@ for c in ['WZ', 'WW']:
 #            plotter.canvas.SaveAs("postfitMVV"+c+"_"+l+"_"+p+".png")
 
 
-
-#plotter=RooPlotter("LNuJJ_topPreFit_HP.root")
-#plotter.prefit()
-#plotter.addContribution("topRes",True,"t#bar{t}",1,1,ROOT.kRed,0,ROOT.kWhite)
-#plotter.addContribution("topNonRes",False,"non-resonant t#bar{t}",1,1,ROOT.kBlack,1001,ROOT.kGreen-5)
+# plotter=RooPlotter("LNuJJ_topPreFit_HP.root")
+# plotter.prefit()
+# plotter.addContribution("topRes",True,"t#bar{t}",1,1,ROOT.kRed,0,ROOT.kWhite)
+# plotter.addContribution("topNonRes",False,"non-resonant t#bar{t}",1,1,ROOT.kBlack,1001,ROOT.kGreen-5)
 #plotter.drawStack("MJ","m_{j} [GeV]","top_mu_HP_13TeV","top_mu_HP_13TeV")
